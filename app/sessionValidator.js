245f39665c176c427bd5686327b59112aee0923a
Drewbie18 <drew.payette@gmail.com>
2016-11-07 19:40:42 -0500

    update

--- app/sessionValidator.js
+++ app/sessionValidator.js
@@ -7,9 +7,19 @@ var cron = require('node-cron');
 
 var validateSession = cron.schedule('* * * * *', function () {
     var date = new Date();
+    console.log('running task every minute- ' + date.toJSON());
+
+    Session.find(function (err, sessions) {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            console.log(sessions);
+
+        }
+    });
 
 
-    console.log('running task every minute' + date.toJSON());
 }, false);
 
 
