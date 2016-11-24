71dfcaa7934f24a51ecdb5c0c5acb71f959d0608
Drewbie18 <drew.payette@gmail.com>
2016-11-23 23:16:58 -0500

    added test facebook scripts

--- app/auth/facebookAuth.js
+++ app/auth/facebookAuth.js
@@ -1,9 +1,21 @@
 /**
  * Created by Drew on 2016-11-22.
+ *
+ * https://scotch.io/tutorials/easy-node-authentication-facebook
+ *
  */
 
 
 var passport = require('passport');
-var FacebookStrategy =require('passport-facebook').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
 
 
+module.exports = function(app){
+
+
+
+
+
+
+
+}
